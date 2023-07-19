223384e6503688b3a490edd20eeefba0a4d29c1e
elaoutarhamza@gmail.com <elaoutarhamza@gmail.com>
2023-07-19 22:44:57 +0200

    http

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
+const https = require('https').Server(app);
+const io = require('socket.io')(https, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
@@ -30,6 +30,6 @@ io.on('connection', (socket) => {
 });
 
 
-http.listen(port, () => {
+https.listen(port, () => {
   console.log(`Socket.IO server running`);
 });
