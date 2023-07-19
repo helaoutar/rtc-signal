ad7f6f341b61e66115ec4eda9f1f3d1c4b77e3c6
Hamza El Aoutar <helaoutar@Hamzas-MacBook-Pro.local>
2023-07-19 16:52:59 +0200

    First

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
+const port = process.env.PORT || 3000;
+
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('signal-offer', msg => {
+    console.log(msg);
+    io.emit('offer', msg);
+  });
+
+  socket.on('signal-answer', msg => {
+    console.log('signal-answer', msg)
+    io.emit('answer', msg);
+  });
+
+  socket.on('send-ice-candidate1', msg => {
+    if (msg) io.emit('ice-candidate1', msg);
+  });
+
+  socket.on('send-ice-candidate2', msg => {
+    if (msg) io.emit('ice-candidate2', msg);
   });
 });
 
+
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+  console.log(`Socket.IO server running`);
 });
