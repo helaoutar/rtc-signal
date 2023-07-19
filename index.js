08fb56eb26fbf22e6c8bc64b86a7d295c9c1cdb6
elaoutarhamza@gmail.com <elaoutarhamza@gmail.com>
2023-07-19 17:07:56 +0200

    force 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const io = require('socket.io')(http, {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 io.on('connection', (socket) => {
   socket.on('signal-offer', msg => {
